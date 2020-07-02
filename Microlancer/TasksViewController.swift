223ad542abebf72c5cf31b0080966c99b9956640
Samuel <samuelcarballogarcia8@gmail.com>
2020-07-02 13:03:04 +0200

    Upload Source Code

---
+++ Microlancer/TasksViewController.swift
@@ -0,0 +1,38 @@
+//
+//  TasksViewController.swift
+//  Microlancer
+//
+//  Created by Samuel on 21/06/20.
+//  Copyright © 2020 Microlancer. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class TasksViewController: UIViewController, WKNavigationDelegate {
+    
+    var webView: WKWebView!
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // 1
+        let url = URL(string: "https://microlancer.io")!
+        webView.load(URLRequest(url: url))
+        
+        // 2
+        let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
+        toolbarItems = [refresh]
+        navigationController?.isToolbarHidden = false
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+
+    }
+
+}
